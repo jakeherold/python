54675c62fe513ae47f5c0d3dd7066b22f32c73e8
jakeherold <jherold.developer@gmail.com>
2018-04-11 20:24:17 -0700

    Got the freaking enumerate stuff down

--- miniprojects/issFinder/iss.py
+++ miniprojects/issFinder/iss.py
@@ -14,7 +14,10 @@ import json
 url = 'http://api.open-notify.org/astros.json'
 response = urllib.request.urlopen(url)
 result = json.loads(response.read())
-print(result)
+
+print("the astronauts are")
+for i, item in enumerate(result['people']):
+    print(item['name'])
 
 
 
