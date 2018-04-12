a34da9a91ca0b731f8557c71cf596ee8de6f702a
jakeherold <jherold.developer@gmail.com>
2018-04-11 20:36:36 -0700

    now with more space station coordinates than evergit add --all!

--- miniprojects/issFinder/iss.py
+++ miniprojects/issFinder/iss.py
@@ -4,21 +4,27 @@
 # Created by Jake Herold
 # If you're reading, utilizing, redistributing, or learning from this code you hearin agree to not judge my terrible code. 
 #
-# Version 0.0.1
+# Version 0.1.0
 
 # Modules
-
 import urllib.request
 import json
 
-url = 'http://api.open-notify.org/astros.json'
-response = urllib.request.urlopen(url)
-result = json.loads(response.read())
+# Get basic API data and assign to a variable to manipulate within the script. 
+astroUrl = 'http://api.open-notify.org/astros.json'
+astroResponse = urllib.request.urlopen(astroUrl)
+astroResult = json.loads(astroResponse.read())
 
-print("The " + str(result['number']) + " astronauts presently in space are: ")
-for i, item in enumerate(result['people']):
-    print(item['name'])
-print("There are currently " + str(result['number']) + " people in space")
+stationUrl = 'http://api.open-notify.org/iss-now.json'
+stationResponse = urllib.request.urlopen(stationUrl)
+stationResult = json.loads(stationResponse.read())
+
+print("The " + str(astroResult['number']) + " astronauts presently in space are: ")
+for i, item in enumerate(astroResult['people']):
+    print("\t" + item['name'] + "is currently on the " + item['craft'])
+
+print("\n" + str(stationResult))
+
 
 
 
