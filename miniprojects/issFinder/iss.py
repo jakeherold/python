59fcd7f2e0f9f637fa34442215568cc521c793c8
jakeherold <jherold.developer@gmail.com>
2018-04-11 19:48:15 -0700

    Added basic info. Mainly commiting to get back into the practice of doing so

---
+++ miniprojects/issFinder/iss.py
@@ -0,0 +1,21 @@
+# International Space Station Finder
+# A mini-project for fun and no profit
+# Created by Jake Herold
+# If you're reading, utilizing, redistributing, or learning from this code you hearin agree to not judge my terrible code. 
+#
+# Version 0.0.1
+
+# Modules
+
+import requests
+
+
+
+
+
+
+
+
+
+
+
