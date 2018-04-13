7d87c145df23f74ea5ef230b34e0392904627334
jakeherold <jherold.developer@gmail.com>
2018-04-12 21:59:02 -0700

    first commit. At this juncture just put in stuff for the md readme and it prints a few rando numbers

---
+++ miniprojects/diceRoller/README.md
@@ -0,0 +1,5 @@
+### Dice Roller
+
+# Goals
+* Inputs that account for the various common dice types in D&D
+* Outputs the sum of all dice
---
+++ miniprojects/diceRoller/roll.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+# A little bitty dice roller by a little bitty python programmer
+# A mini-project for fun and no profit
+# Created by Jake Herold
+# If you're reading, utilizing, redistributing, or learning from this code you hearin agree to not judge my terrible code. 
+#
+# Version 0.0.1
+
+import random
+import sys
+
+
+
+for x in range(10):
+  print(random.randint(1,101))
