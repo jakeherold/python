b57bd95603ec7a1842561e00a24cc21a55749264
Jake Herold <jherold.developer@gmail.com>
2018-04-12 21:21:53 -0700

    after being harassed about my use of urllib.request i opted to swap it out for the requests library instead

--- miniprojects/issFinder/iss.py
+++ miniprojects/issFinder/iss.py
@@ -4,22 +4,22 @@
 # Created by Jake Herold
 # If you're reading, utilizing, redistributing, or learning from this code you hearin agree to not judge my terrible code. 
 #
-# Version 0.1.2
+# Version 0.1.3
 
 # Modules
-import urllib.request
+import requests
 import json
 import time
 
 # Get basic Astronaut API data and assign to a variable to manipulate within the script. 
 astroUrl = 'http://api.open-notify.org/astros.json'
-astroResponse = urllib.request.urlopen(astroUrl)
-astroResult = json.loads(astroResponse.read())
+astroResponse = requests.get(astroUrl)
+astroResult = astroResponse.json()
 
 # Get basic ISS Location data and assign each component to a variable
 stationUrl = 'http://api.open-notify.org/iss-now.json'
-stationResponse = urllib.request.urlopen(stationUrl)
-stationResult = json.loads(stationResponse.read())
+stationResponse = requests.get(stationUrl)
+stationResult = stationResponse.json()
 location = stationResult['iss_position']
 lat = stationResult['iss_position']['latitude']
 lon = stationResult['iss_position']['longitude']
@@ -30,20 +30,15 @@ passUrl = 'http://api.open-notify.org/iss-pass.json'
 jakeLat = 45.4824777
 jakeLon = -122.7356999
 passUrlWithCoords = passUrl + '?lat=' + str(jakeLat) + '&lon=' + str(jakeLon)
-passResponse = urllib.request.urlopen(passUrlWithCoords)
-passResult = json.loads(passResponse.read())
+passResponse = requests.get(passUrlWithCoords)
+passResult = passResponse.json()
 passOverTime = passResult['response'][1]['risetime']
 
+
+# Outputs
 print("The " + str(astroResult['number']) + " astronauts presently in space are: ")
 for i, item in enumerate(astroResult['people']):
     print("\t" + item['name'] + " (" + item['craft'] + ")")
-
 print("The ISS's latitude is currently: " + lat)
 print("The ISS's longitude is currently: " + lon)
-
 print("ISS passes Portland, OR at: " + time.ctime(passOverTime))
-
-
-
-
-
